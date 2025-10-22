5893324e3c63598a0f24b4d2213a50e89aa5dfe8
Ricardo <romeromedinar612@gmail.com>
2025-10-21 21:57:43 -0600

    🚀 RESET COMPLETO: Inicio TortilleriaV2 desde cero

--- resources/views/dashboard/despacho.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('title', 'Dashboard Despacho')
-
-@section('content')
-    <div class="d-flex justify-content-center align-items-center" style="min-height: 60vh;">
-        <h1 class="text-3xl font-bold">Bienvenido Despachador</h1>
-    </div>
-@endsection
